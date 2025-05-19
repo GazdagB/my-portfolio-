4fdba7d27762c075930115685e8853c75ec69d29
GazdagB <gazdagbal@gmail.com>
2025-05-19 21:31:50 +0200

    💄 Adds Layout correction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar></Navbar>
-        {children}
+        <main className="ps-[100px]">
+          {children}
+        </main>
       </body>
     </html>
   );
