a4394bc7009524b58f00070de094305e67ab46b1
GazdagB <gazdagbal@gmail.com>
2025-05-19 21:41:27 +0200

    ✨ Adds icons for page navigation

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { AiOutlineHome } from "react-icons/ai";
+import { BiUser } from "react-icons/bi";
+import { TbBriefcase2 } from "react-icons/tb";
+import { LuContactRound } from "react-icons/lu";
+import { IoNewspaperOutline } from "react-icons/io5";
+
+
 
 const Navbar = () => {
   return (
-    <div className='bg-charcoal'>Navbar</div>
+    <div className='flex flex-col items-center  h-full fixed w-[100px] bg-off-white border-r-2'>
+        <img className='w-[50px]' src="/gb_logo.svg" alt="" />
+        {/* Page Menu */}
+        <div className='text-3xl'>
+            <AiOutlineHome />
+            <BiUser />
+            <TbBriefcase2 />
+            <LuContactRound />
+            <IoNewspaperOutline />
+
+        </div>
+
+        {/* Social Menu */}
+        <div></div>
+
+        {/* Dark Mode Toggle */}
+
+    </div>
   )
 }
 
