94486cf09213b041147a5ff54d6a8cd217e833d7
GazdagB <gazdagbal@gmail.com>
2025-05-20 21:44:52 +0200

    ✨ Adds open-close  logic and conditional rendering for tootltips

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,8 +14,12 @@ import { ImYoutube } from 'react-icons/im'
 import { FaSquareXTwitter } from 'react-icons/fa6'
 import { Tooltip } from 'react-tooltip'
 import 'react-tooltip/dist/react-tooltip.css'
+import { useState } from 'react'
 
 const Navbar = () => {
+
+    const [isOpen,setIsOpen] = useState(false); 
+    
   return (
     <div className='flex flex-col items-center justify-between py-15 h-full fixed w-[100px] bg-off-white border-r-2'>
       <img
@@ -29,27 +33,27 @@ const Navbar = () => {
         <AiOutlineHome
           data-tooltip-id="tooltip-home"
           data-tooltip-content="Home"
-          className="cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0 cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
         />
         <BiUser
           data-tooltip-id="tooltip-user"
           data-tooltip-content="About"
-          className="cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0  cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
         />
         <TbBriefcase2
           data-tooltip-id="tooltip-work"
           data-tooltip-content="Work"
-          className="cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0  cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
         />
         <LuContactRound
           data-tooltip-id="tooltip-contact"
           data-tooltip-content="Contact"
-          className="cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0  cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
         />
         <IoNewspaperOutline
           data-tooltip-id="tooltip-blog"
           data-tooltip-content="Blog"
-          className="cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0  cursor-pointer hover:text-secondary hover:scale-115 transition-all ease-in-out"
         />
       </div>
 
@@ -58,22 +62,22 @@ const Navbar = () => {
         <FaGithub
           data-tooltip-id="tooltip-github"
           data-tooltip-content="GitHub"
-          className="cursor-pointer hover:text-primary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0  cursor-pointer hover:text-primary hover:scale-115 transition-all ease-in-out"
         />
         <ImYoutube
           data-tooltip-id="tooltip-youtube"
           data-tooltip-content="YouTube"
-          className="cursor-pointer hover:text-primary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0  cursor-pointer hover:text-primary hover:scale-115 transition-all ease-in-out"
         />
         <FaSquareXTwitter
           data-tooltip-id="tooltip-twitter"
           data-tooltip-content="Twitter"
-          className="cursor-pointer hover:text-primary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0  cursor-pointer hover:text-primary hover:scale-115 transition-all ease-in-out"
         />
         <FaInstagramSquare
           data-tooltip-id="tooltip-instagram"
           data-tooltip-content="Instagram"
-          className="cursor-pointer hover:text-primary hover:scale-115 transition-all ease-in-out"
+          className="focus:outline-none focus-visible:ring-0  cursor-pointer hover:text-primary hover:scale-115 transition-all ease-in-out"
         />
       </div>
 
@@ -81,20 +85,24 @@ const Navbar = () => {
       <AiFillMoon
         data-tooltip-id="tooltip-darkmode"
         data-tooltip-content="Toggle Dark Mode"
-        className="text-3xl cursor-pointer text-[#EDBD39] hover:rotate-[20deg] transition-all ease-in hover:scale-125"
+        className="focus:outline-none focus-visible:ring-0  text-3xl cursor-pointer text-[#EDBD39] hover:rotate-[20deg] transition-all ease-in hover:scale-125"
       />
 
       {/* Tooltips */}
-      <Tooltip place='right' id="tooltip-home" />
-      <Tooltip place='right' id="tooltip-user" />
-      <Tooltip place='right' id="tooltip-work" />
-      <Tooltip place='right' id="tooltip-contact" />
-      <Tooltip  place='right' id="tooltip-blog" />
-      <Tooltip place='right' id="tooltip-github" />
-      <Tooltip place='right' id="tooltip-youtube" />
-      <Tooltip place='right' id="tooltip-twitter" />
-      <Tooltip place='right' id="tooltip-instagram" />
-      <Tooltip place='right' id="tooltip-darkmode" />
+     
+      <Tooltip hidden={isOpen} place='right' id="tooltip-home" />
+      <Tooltip hidden={isOpen} place='right' id="tooltip-user" />
+      <Tooltip hidden={isOpen} place='right' id="tooltip-work" />
+      <Tooltip hidden={isOpen} place='right' id="tooltip-contact" />
+      <Tooltip hidden={isOpen}  place='right' id="tooltip-blog" />
+      <Tooltip hidden={isOpen} place='right' id="tooltip-github" />
+      <Tooltip hidden={isOpen} place='right' id="tooltip-youtube" />
+      <Tooltip hidden={isOpen} place='right' id="tooltip-twitter" />
+      <Tooltip hidden={isOpen} place='right' id="tooltip-instagram" />
+      <Tooltip hidden={isOpen} place='right' id="tooltip-darkmode" />
+    
+
+      
     </div>
   )
 }
