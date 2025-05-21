8e733d4bd3408d0e651d16c9447168bc6ef76335
GazdagB <gazdagbal@gmail.com>
2025-05-21 21:17:10 +0200

    ✨ Adds functional timer

--- src/app/components/Forcast.tsx
+++ src/app/components/Forcast.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { HiSun } from "react-icons/hi2";
+import Timer from "./Timer"
 
 const Forcast = () => {
   return (
     <div className='flex gap-2 items-center'>
         <HiSun className='text-amber-400 text-lg'></HiSun>
         <p>32° Mannheim,</p>
-        <p className='font-bold'>19:23:21</p>
+        <Timer></Timer>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import Forcast from "./components/Forcast";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="items-center justify-items-center min-h-screen font-sans">
       {/* Top Bar */}
         <div className="flex items-center gap-10 justify-self-end me-20 pt-10">
           <div className="bg-[#ECECEC] flex  px-5 py-2 font-bold rounded-full cursor-pointer">
