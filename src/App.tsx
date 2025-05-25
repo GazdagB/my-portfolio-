2b9e5a00b3a39c3dc9c45c48f74557079edca3f7
GazdagB <gazdagbal@gmail.com>
2025-05-25 12:12:00 +0200

    ✨ Weather tracking feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import gazdag from "./assets/gazdag.svg"
 import { GoArrowUpRight } from "react-icons/go";
 import hero from "./assets/hero-image.svg"
 import arrow from "./assets/arrow.png"
+import mash from "./assets/mash.svg"
 
 function App() {
   return (
-    <div className="items-center justify-items-center min-h-screen font-sans">
+    <div  style={{background: `url(${mash})`, backgroundRepeat: "no-repeat", backgroundPosition: "bottom", backgroundSize: "contain"}} className="items-center justify-items-center min-h-screen font-sans bg-cover ">
       {/* Top Bar */}
         <div className="flex items-center gap-10 justify-self-end me-20 pt-10">
           <div className="bg-[#ECECEC] flex  px-5 py-2 font-bold rounded-full cursor-pointer">
@@ -14,9 +15,9 @@ function App() {
           </div>
           <Forcast></Forcast>
         </div>
-        <div className="flex items-center justify-between w-full max-w-[1000px] gap-10 min-h-[80vh]  ">
+        <div className="flex items-center justify-between w-full max-w-[1200px] gap-10 min-h-[80vh]  ">
           {/* Left */}
-          <div className="relative">
+          <div className="relative ps-[100px]">
             <div className="text-[30px] flex flex-col mb-5">
               <p className="mb-[-15px] text-primary font-[500] font-t-sans">Hi!</p>
               <p className="font-[400] font-t-sans">MY NAME IS:</p>
@@ -39,11 +40,14 @@ function App() {
 
           {/* Hero Right */}
           <div className="relative">
+              <p className="text-[#898989] ms-[-40px] text-lg">&lt;img src="./hero-image.webp"&gt;</p>
               <img width={400} src={hero} alt="" />
               <div className="w-25 h-25 rounded-3xl flex items-center justify-center bg-charcoal absolute bottom-0 text-white text-[60px] cursor-pointer">
                 <GoArrowUpRight />
               </div>
+              <p className="text-[#898989] text-lg absolute right-0 bottom-[-40px]">//Image of Gazdag Balázs</p>
           </div>
+          
         </div>
     </div>
   );
--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,14 +1,75 @@
-import { HiSun } from "react-icons/hi2";
-import Timer from "./Timer"
+import Timer from "./Timer";
+import { useState, useEffect } from "react";
+import { FaSpinner } from "react-icons/fa";
+
+type WeatherData = {
+  current: {
+    temp_c: number;
+    condition: { icon: string };
+  };
+};
 
 const Forcast = () => {
-  return (
-    <div className='flex gap-2 items-center'>
-        <HiSun className='text-amber-400 text-lg'></HiSun>
-        <p>32° Mannheim,</p>
-        <Timer></Timer>
-    </div>
-  )
-}
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-export default Forcast
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+  async function fetchWeather() {
+    const url =
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Mannheim&aqi=no`;
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setWeather(data);
+      setError(false);
+    } catch (error: unknown) {
+      setError(true);
+      if (error instanceof Error) {
+        console.error(error.message);
+      } else {
+        console.error(error);
+      }
+    } finally {
+      setLoading(false); 
+    }
+  }
+
+  useEffect(() => {
+    fetchWeather()
+  }, []);
+
+  return (
+    <div className="flex gap-2 items-center">
+      {loading ? (
+        <div className="flex items-center justify-center gap-1 text-gray-500">
+          <FaSpinner className="h-5 w-5 animate-spin" />
+          Loading Weather...
+        </div>
+      ) : error ? (
+        <p className="">Couldn’t load weather 😕</p>
+      ) : (
+        weather && (
+          <>
+            <img
+              className="h-8"
+              src={weather.current.condition.icon}
+              alt="weather icon"
+            />
+            <p>{weather.current.temp_c}° Mannheim,</p>
+          </>
+        )
+      )}
+      <Timer />
+    </div>
+  );
+};
+
+export default Forcast;
