3d7caad630da6e71cc46702a020d71fb25c152cb
GazdagB <gazdagbal@gmail.com>
2025-05-30 23:40:16 +0200

    ✨ Adds about page to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Facts from "./components/Facts";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
    <div className="ps-[100px]">
       <Hero/>
       <Facts/>
+      <About/>
    </div>
   );
 }
