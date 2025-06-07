0abe288be671f148fb9e264fa2a815acd9aea5b0
GazdagB <gazdagbal@gmail.com>
2025-06-07 23:54:03 +0200

    💬 Adds text for work section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,7 +2,12 @@
 // TODO: Add Mocked Work Pieces
 const Work = () => {
   return (
-    <div>Work</div>
+    <div className="flex items-center justify-center">
+        <div className="flex flex-col w-[450px] items-center">
+            <h2 className="text-4xl font-bold items-center">My Work</h2>
+            <p className="text-center">Check my latest portfolio work projects. If you have any questions feel free to ask me for more information.</p>
+        </div>
+    </div>
   )
 }
 
