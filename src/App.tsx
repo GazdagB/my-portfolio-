0366a67ce3f1a85eb5b0575dba75561cf36be1ab
GazdagB <gazdagbal@gmail.com>
2025-06-16 23:39:10 +0200

    💡 Comment out things for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Facts from "./components/Facts";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Work from "./components/Work";
+// import Facts from "./components/Facts";
+// import Hero from "./components/Hero";
+// import About from "./components/About";
+// import Work from "./components/Work";
 import Construction from "./components/Construction/Construction";
 
 function App() {
