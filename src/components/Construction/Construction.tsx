46f0414b03cf93238fd07d57aa9e58a8188bb839
GazdagB <gazdagbal@gmail.com>
2025-06-17 18:43:55 +0200

    💄 Fix CSS realated deployement problems

--- src/components/Construction/Construction.tsx
+++ src/components/Construction/Construction.tsx
@@ -10,11 +10,11 @@ const Construction = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw]">
-        <p className="text-center font-t-sans text-2xl  lg-text-5xl uppercase text-charcoal mb-5">
+        <p className="text-center font-t-sans text-2xl lg:text-5xl uppercase text-charcoal mb-5">
          Sorry! Website <br /> Under Construction!
         </p>
         <ProgressBar precent={20} />
-        <img className="w-100 mb-15 px-10" src={GazdagSLogo} alt="Gazdag Studio Logo" />
+        <img className="w-100 mb-16 px-10" src={GazdagSLogo} alt="Gazdag Studio Logo" />
         <div className="text-3xl items-center justify-center flex gap-3">
           <Link to={"https://www.youtube.com/@GazdagStudio"} target="_blank">
             <FaYoutube />
