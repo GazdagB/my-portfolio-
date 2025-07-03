cc3a7b5f01f6a878aa2d37a7d2e209dd09f4a8d6
GazdagB <gazdagbal@gmail.com>
2025-07-03 20:44:08 +0200

    🚧 Restructuring for dev enviroment

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-// import Facts from "./components/Facts";
-// import Hero from "./components/Hero";
-// import About from "./components/About";
-// import Work from "./components/Work";
-import Construction from "./components/Construction/Construction";
+import Facts from "./components/Facts";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Work from "./components/Work";
+//import Construction from "./components/Construction/Construction";
 
 function App() {
   return (
    <div className="">
-    <Construction></Construction>
-      {/* <Hero/>
+    {/* <Construction></Construction> */}
+      <Hero/>
       <Facts/>
       <About/>
-      <Work/> */}
+      <Work/> 
    </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-// import Navbar from './components/Navbar.tsx'
+import Navbar from './components/Navbar.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      {/* <Navbar/> */}
+     <Navbar/>
       <App />
     </BrowserRouter>
   </StrictMode>,
